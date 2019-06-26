bf04db2ecce2924fbbbb0dcd4a7b441f9f7a71df
Kurt Bauer <kurtbauer55@protonmail.com>
2019-06-26 18:28:38 -0400

    add notes on go cli commands

--- helloworld/main.go
+++ helloworld/main.go
@@ -11,6 +11,14 @@ func main() {
 // how do we run our code in the project?
 //go run -your file.go-
 
+//go cli commands
+// go build -> compiles go source code files
+// go run -> compiles or executes, one or two files
+// go fmt -> formats all the code in each file in the current directory
+// go install -> compiles and installs a package
+// go get -> compiles the raw source code of someone else's package
+// go test -> run any tests associated with the current project
+
 //what does 'package main' mean?
 
 //what does 'import fmt' mean?
