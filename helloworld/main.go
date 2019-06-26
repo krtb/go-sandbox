02835ed21cf5df332c4dcbeab4f6dc898036e39d
Kurt Bauer <kurtbauer55@protonmail.com>
2019-06-26 18:25:17 -0400

    write line of go

---
+++ helloworld/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi, There!")
+}
+
+//print L N, println
+
+// how do we run our code in the project?
+//go run -your file.go-
+
+//what does 'package main' mean?
+
+//what does 'import fmt' mean?
+
+//what is a func?
+
+//how is the 'main.go' file organized?
