24ba6a86ab1f28bc0e80d8c53c08f39d610ba012
Kurt Bauer <kurtbauer55@protonmail.com>
2019-06-27 15:35:43 -0400

    refactor to include the newDeck func

--- Cards/main.go
+++ Cards/main.go
@@ -1,13 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := deck{"Ace of diamonds", newCard()}
-	cards = append(cards, "Six of spades")
-	//append returns a new array, does not modify cards() slice
+	cards := newDeck()
 
 	cards.print()
-
-	fmt.Println(cards)
 }
