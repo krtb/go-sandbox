eb0a98aa059ce90cabbc8bd8481e3f1a411aa114
Kurt Bauer <kurtbauer55@protonmail.com>
2019-06-27 12:32:06 -0400

    clean up code, add new function

--- Cards/main.go
+++ Cards/main.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	//var card string = "Ace of Spades"
 	card := "Ace of Spades"
-	//only declare once, when re-assigning don't need semicolon
+
 	fmt.Println(card)
+}
+
+func newCard() {
+
 }
